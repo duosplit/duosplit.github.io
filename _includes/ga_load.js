edbf65fb028e9978bc502851e572787d2f67b2ef
Mathieu Provencher <3145731+matprov@users.noreply.github.com>
2023-06-09 15:35:07 -0400

    gdpr gtag optional load

---
+++ _includes/ga_load.js
@@ -0,0 +1,26 @@
+function loadJS(FILE_URL, async = true) {
+  let scriptEle = document.createElement("script");
+
+  scriptEle.setAttribute("src", FILE_URL);
+  scriptEle.setAttribute("type", "text/javascript");
+  scriptEle.setAttribute("async", async);
+
+  document.body.appendChild(scriptEle);
+
+  // success event 
+  scriptEle.addEventListener("load", () => {
+    console.log("File loaded")
+  });
+   // error event
+  scriptEle.addEventListener("error", (ev) => {
+    console.log("Error on loading file", ev);
+  });
+}
+
+loadJS("https://www.googletagmanager.com/gtag/js?id=G-C1BFCNLTX4", true);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-C1BFCNLTX4');
